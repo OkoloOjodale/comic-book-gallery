a7c049f74d78955b6c11df3460a3698ffa79aa37
OkoloOjodale <okolojohnojo@gmail.com>
2023-01-21 11:24:41 +0100

    Added Comic Book Detail view

--- src/ComicGalleryBooks/ComicGalleryBooks/Controllers/ComicBooksController.cs
+++ src/ComicGalleryBooks/ComicGalleryBooks/Controllers/ComicBooksController.cs
@@ -10,13 +10,7 @@ namespace ComicGalleryBooks.Controllers
     {
         public ActionResult Detail()
         {
-            if (DateTime.Today.DayOfWeek == DayOfWeek.Saturday)
-            {
-                return Redirect("/");
-            }
-
-            return Content("Hello from the comic books controller"); 
-            
+            return View();
         }
     }
 }
